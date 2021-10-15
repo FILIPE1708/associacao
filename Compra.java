8aefd59e54e5a1cb58812338580f638f8f8ca61c
Filipe Cavalcante <83718838+FILIPE1708@users.noreply.github.com>
2021-10-15 11:21:07 -0300

    Trabalho de associação

---
+++ Compra.java
@@ -0,0 +1,42 @@
+package Associacao;
+
+import java.util.Date;
+
+public class Compra {
+    private int codigo;
+    private String notaFiscal;
+    private Date data;
+    private Item[] itens = new Item[10];
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNotaFiscal() {
+        return notaFiscal;
+    }
+
+    public void setNotaFiscal(String notaFiscal) {
+        this.notaFiscal = notaFiscal;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Item[] getItens() {
+        return itens;
+    }
+
+    public void setItens(Item[] itens) {
+        this.itens = itens;
+    }
+}
